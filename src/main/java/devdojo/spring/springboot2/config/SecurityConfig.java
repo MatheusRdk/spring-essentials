2e2042d5ca33f879d012f69c33d4690f9a0de22e
MatheusRdk <matheus_rudek@hotmail.com>
2023-06-18 11:38:27 -0300

    spring security

--- src/main/java/devdojo/spring/springboot2/config/SecurityConfig.java
+++ src/main/java/devdojo/spring/springboot2/config/SecurityConfig.java
@@ -3,7 +3,10 @@ package devdojo.spring.springboot2.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -14,12 +17,13 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
 @Log4j2
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests((authz) -> authz
                 .anyRequest().authenticated()
         ).httpBasic(withDefaults());
         return http.build();
